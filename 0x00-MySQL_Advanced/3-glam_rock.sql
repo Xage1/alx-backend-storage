c89069a5ca6f971734f5493d46e6a515a1aaba26
Xage1 <kennethkiarie555@gmail.com>
2024-03-21 12:44:00 +0000

    This is it

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,4 @@
+-- SQL script that lists all bands with Glam rock as their main style
+
+SELECT band_name, COALESCE(SPLIT, 2022) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
